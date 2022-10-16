995e02e0f28e2e24cd8caf5968cf8f4388ff7ce0
edwinnambaje <edwinnambaje@gmail.com>
2022-10-16 20:42:37 +0200

    first commit

--- App.js
+++ App.js
@@ -1,12 +1,98 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View ,FlatList,TouchableOpacity} from 'react-native';
+import React,{useState} from 'react';
 
 export default function App() {
+  const[todos,setTodos]=useState(
+    [
+    {
+      "userId": 1,
+      "id": 1,
+      "title": "delectus aut autem",
+      "completed": false
+    },
+    {
+      "userId": 1,
+      "id": 2,
+      "title": "quis ut nam facilis et officia qui",
+      "completed": false
+    },
+    {
+      "userId": 1,
+      "id": 3,
+      "title": "fugiat veniam minus",
+      "completed": false
+    },
+    {
+      "userId": 1,
+      "id": 4,
+      "title": "et porro tempora",
+      "completed": true
+    },
+    {
+      "userId": 1,
+      "id": 5,
+      "title": "laboriosam mollitia et enim quasi adipisci quia provident illum",
+      "completed": false
+    },
+    {
+      "userId": 1,
+      "id": 6,
+      "title": "qui ullam ratione quibusdam voluptatem quia omnis",
+      "completed": false
+    },
+    {
+      "userId": 1,
+      "id": 7,
+      "title": "illo expedita consequatur quia in",
+      "completed": false
+    },
+    {
+      "userId": 1,
+      "id": 8,
+      "title": "quo adipisci enim quam ut ab",
+      "completed": true
+    },
+    {
+      "userId": 1,
+      "id": 9,
+      "title": "molestiae perspiciatis ipsa",
+      "completed": false
+    },
+    {
+      "userId": 1,
+      "id": 10,
+      "title": "illo est ratione doloremque quia maiores aut",
+      "completed": true
+    }
+  ])
+  const pressHandler=(id)=>{
+    const selectedItem=(todos.filter(todo=>todo.id===id))[0]
+    alert(selectedItem.title)
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <FlatList
+    numColumns={2}
+      data={todos}
+      renderItem={({item})=>(
+        <TouchableOpacity onPress={()=>pressHandler(item.id)}>
+          <Text style={styles.todo}>{item.title}</Text>
+        </TouchableOpacity>
+        
+      )}
+      />
+      </View>
+    // />
+    //   // <ScrollView>
+    //   // {todos.map((todo)=>{
+      //   return(
+      //     <View key={todo.id} style={styles.todo}>
+      //       <Text>{todo.title}</Text>
+      //     </View>
+      //   )
+      // })}
+      //  </ScrollView>
+    // </View>
   );
 }
 
@@ -14,7 +100,14 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    // alignItems: 'center',
+    // justifyContent: 'center',
   },
+  todo:{
+    margin:10,
+    backgroundColor:"deeppink",
+    color:"white",
+    fontSize:10,
+    padding:10,
+  }
 });
